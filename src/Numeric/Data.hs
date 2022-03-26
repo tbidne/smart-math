32eaebc383168c1962cfea48ba1eb4fea6a49248
Tommy Bidne <tbidne@protonmail.com>
2022-03-26 18:43:31 +1300

    Export Interval from Numeric.Data

--- src/Numeric/Data.hs
+++ src/Numeric/Data.hs
@@ -4,6 +4,7 @@
 -- @since 0.1.0.0
 module Numeric.Data
   ( module Numeric.Data.Fraction,
+    module Numeric.Data.Interval,
     module Numeric.Data.ModN,
     module Numeric.Data.ModP,
     module Numeric.Data.NonNegative,
@@ -13,6 +14,7 @@ module Numeric.Data
 where
 
 import Numeric.Data.Fraction
+import Numeric.Data.Interval
 import Numeric.Data.ModN
 import Numeric.Data.ModP
 import Numeric.Data.NonNegative
