42da2a0f4b41fc012c8247c724a3b4bad02daf75
Tommy Bidne <tbidne@protonmail.com>
2024-07-25 13:36:00 +1200

    Test larger Int/Word sizes with arithmoi

--- test/unit/Test/Data/ModP.hs
+++ test/unit/Test/Data/ModP.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE CPP #-}
 
 module Test.Data.ModP (props) where
 
@@ -26,17 +27,30 @@ props :: TestTree
 props =
   testGroup
     "Numeric.Data.ModP"
-    [ testUnsafe,
-      int8Props,
-      int16Props,
-      integerProps,
-      word8Props,
-      word16Props,
-      naturalProps,
-      elimProps,
-      showSpecs,
-      displaySpecs
-    ]
+    tests
+  where
+    tests =
+      testUnsafe
+        : intTypeProps
+        ++ wordTypeProps
+        ++ [ elimProps,
+             showSpecs,
+             displaySpecs
+           ]
+
+    intTypeProps =
+      [ int8Props,
+        int16Props
+      ]
+        ++ extraIntProps
+        ++ [integerProps]
+
+    wordTypeProps =
+      [ word8Props,
+        word16Props
+      ]
+        ++ extraWordProps
+        ++ [naturalProps]
 
 testUnsafe :: TestTree
 testUnsafe = testCase "Test unsafeModP" $ do
@@ -585,3 +599,354 @@ displaySpecs :: TestTree
 displaySpecs = testCase "Displays ModP" $ do
   "2 (mod 7)" @=? D.display (ModP.unsafeModP @7 @Integer 2)
   "9 (mod 13)" @=? D.display (ModP.unsafeModP @13 @Integer 22)
+
+extraIntProps :: [TestTree]
+extraWordProps :: [TestTree]
+
+#if USE_ARITHMOI
+
+extraIntProps =
+  [ int32Props,
+    int64Props,
+    intProps
+  ]
+
+int32Props :: TestTree
+int32Props =
+  testGroup
+    "Int32"
+    [ mkModPInt32,
+      mkModPFailLargeInt32,
+      mkModPFailCompositeInt32,
+      addTotalInt32,
+      subTotalInt32,
+      multTotalInt32,
+      divTotalInt32,
+      invertInt32,
+      boundedVals @2147483629 @Natural
+    ]
+
+mkModPInt32 :: TestTree
+mkModPInt32 =
+  testPropertyCompat "mkModP x" "mkModNInt32" $
+    mkModP' @2147483629 @Int32
+
+mkModPFailLargeInt32 :: TestTree
+mkModPFailLargeInt32 =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeInt32" $
+    mkModPFailSize @2147483659 @Int32 "Int32" "2147483647" "2147483659"
+
+mkModPFailCompositeInt32 :: TestTree
+mkModPFailCompositeInt32 =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeInt32" $
+    mkModPFailComposite @2147483627 @Int32 "2147483627"
+
+addTotalInt32 :: TestTree
+addTotalInt32 =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalInt32" $
+    addTotal' @2147483629 @Int32
+
+subTotalInt32 :: TestTree
+subTotalInt32 =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalInt32" $
+    subTotal' @2147483629 @Int32
+
+multTotalInt32 :: TestTree
+multTotalInt32 =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalInt32" $
+    multTotal' @2147483629 @Int32
+
+divTotalInt32 :: TestTree
+divTotalInt32 =
+  testPropertyCompat "(.%.) implements modular division" "divTotalInt32" $
+    divTotal' @2147483629 @Int32
+
+invertInt32 :: TestTree
+invertInt32 =
+  testPropertyCompat "1 == x * invert x" "invertInt32" $
+    invert' @2147483629 @Int32
+
+int64Props :: TestTree
+int64Props =
+  testGroup
+    "Int64"
+    [ mkModPInt64,
+      mkModPFailLargeInt64,
+      mkModPFailCompositeInt64,
+      addTotalInt64,
+      subTotalInt64,
+      multTotalInt64,
+      divTotalInt64,
+      invertInt64,
+      boundedVals @9223372036854775783 @Natural
+    ]
+
+mkModPInt64 :: TestTree
+mkModPInt64 =
+  testPropertyCompat "mkModP x" "mkModNInt64" $
+    mkModP' @9223372036854775783 @Int64
+
+mkModPFailLargeInt64 :: TestTree
+mkModPFailLargeInt64 =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeInt64" $
+    mkModPFailSize @9223372036854775837 @Int64 "Int64" "9223372036854775807" "9223372036854775837"
+
+mkModPFailCompositeInt64 :: TestTree
+mkModPFailCompositeInt64 =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeInt64" $
+    mkModPFailComposite @9223372036854775781 @Int64 "9223372036854775781"
+
+addTotalInt64 :: TestTree
+addTotalInt64 =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalInt64" $
+    addTotal' @9223372036854775783 @Int64
+
+subTotalInt64 :: TestTree
+subTotalInt64 =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalInt64" $
+    subTotal' @9223372036854775783 @Int64
+
+multTotalInt64 :: TestTree
+multTotalInt64 =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalInt64" $
+    multTotal' @9223372036854775783 @Int64
+
+divTotalInt64 :: TestTree
+divTotalInt64 =
+  testPropertyCompat "(.%.) implements modular division" "divTotalInt64" $
+    divTotal' @9223372036854775783 @Int64
+
+invertInt64 :: TestTree
+invertInt64 =
+  testPropertyCompat "1 == x * invert x" "invertInt64" $
+    invert' @9223372036854775783 @Int64
+
+intProps :: TestTree
+intProps =
+  testGroup
+    "Int"
+    [ mkModPInt,
+      mkModPFailLargeInt,
+      mkModPFailCompositeInt,
+      addTotalInt,
+      subTotalInt,
+      multTotalInt,
+      divTotalInt,
+      invertInt
+    ]
+
+mkModPInt :: TestTree
+mkModPInt =
+  testPropertyCompat "mkModP x" "mkModNInt" $
+    mkModP' @65521 @Int
+
+mkModPFailLargeInt :: TestTree
+mkModPFailLargeInt =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeInt" $
+    mkModPFailSize @9223372036854775837 @Int "Int" "9223372036854775807" "9223372036854775837"
+
+mkModPFailCompositeInt :: TestTree
+mkModPFailCompositeInt =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeInt" $
+    mkModPFailComposite @65517 @Int "65517"
+
+addTotalInt :: TestTree
+addTotalInt =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalInt" $
+    addTotal' @65521 @Int
+
+subTotalInt :: TestTree
+subTotalInt =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalInt" $
+    subTotal' @65521 @Int
+
+multTotalInt :: TestTree
+multTotalInt =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalInt" $
+    multTotal' @65521 @Int
+
+divTotalInt :: TestTree
+divTotalInt =
+  testPropertyCompat "(.%.) implements modular division" "divTotalInt" $
+    divTotal' @65521 @Int
+
+invertInt :: TestTree
+invertInt =
+  testPropertyCompat "1 == x * invert x" "invertInt" $
+    invert' @65521 @Int
+
+extraWordProps =
+  [ word32Props,
+    word64Props,
+    wordProps
+  ]
+
+word32Props :: TestTree
+word32Props =
+  testGroup
+    "Word32"
+    [ mkModPWord32,
+      mkModPFailLargeWord32,
+      mkModPFailCompositeWord32,
+      addTotalWord32,
+      subTotalWord32,
+      multTotalWord32,
+      divTotalWord32,
+      invertWord32
+    ]
+
+mkModPWord32 :: TestTree
+mkModPWord32 =
+  testPropertyCompat "mkModP x" "mkModNWord32" $
+    mkModP' @4294967291 @Word32
+
+mkModPFailLargeWord32 :: TestTree
+mkModPFailLargeWord32 =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeWord32" $
+    mkModPFailSize @4294967311 @Word32 "Word32" "4294967295" "4294967311"
+
+mkModPFailCompositeWord32 :: TestTree
+mkModPFailCompositeWord32 =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeWord32" $
+    mkModPFailComposite @4294967289 @Word32 "4294967289"
+
+addTotalWord32 :: TestTree
+addTotalWord32 =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalWord32" $
+    addTotal' @4294967291 @Word32
+
+subTotalWord32 :: TestTree
+subTotalWord32 =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalWord32" $
+    subTotal' @4294967291 @Word32
+
+multTotalWord32 :: TestTree
+multTotalWord32 =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalWord32" $
+    multTotal' @4294967291 @Word32
+
+divTotalWord32 :: TestTree
+divTotalWord32 =
+  testPropertyCompat "(.%.) implements modular division" "divTotalWord32" $
+    divTotal' @4294967291 @Word32
+
+invertWord32 :: TestTree
+invertWord32 =
+  testPropertyCompat "1 == x * invert x" "invertWord32" $
+    invert' @4294967291 @Word32
+
+word64Props :: TestTree
+word64Props =
+  testGroup
+    "Word64"
+    [ mkModPWord64,
+      mkModPFailLargeWord64,
+      mkModPFailCompositeWord64,
+      addTotalWord64,
+      subTotalWord64,
+      multTotalWord64,
+      divTotalWord64,
+      invertWord64
+    ]
+
+mkModPWord64 :: TestTree
+mkModPWord64 =
+  testPropertyCompat "mkModP x" "mkModNWord64" $
+    mkModP' @18446744073709551557 @Word64
+
+mkModPFailLargeWord64 :: TestTree
+mkModPFailLargeWord64 =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeWord64" $
+    mkModPFailSize @18446744073709551629 @Word64 "Word64" "18446744073709551615" "18446744073709551629"
+
+mkModPFailCompositeWord64 :: TestTree
+mkModPFailCompositeWord64 =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeWord64" $
+    mkModPFailComposite @18446744073709551555 @Word64 "18446744073709551555"
+
+addTotalWord64 :: TestTree
+addTotalWord64 =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalWord64" $
+    addTotal' @18446744073709551557 @Word64
+
+subTotalWord64 :: TestTree
+subTotalWord64 =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalWord64" $
+    subTotal' @18446744073709551557 @Word64
+
+multTotalWord64 :: TestTree
+multTotalWord64 =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalWord64" $
+    multTotal' @18446744073709551557 @Word64
+
+divTotalWord64 :: TestTree
+divTotalWord64 =
+  testPropertyCompat "(.%.) implements modular division" "divTotalWord64" $
+    divTotal' @18446744073709551557 @Word64
+
+invertWord64 :: TestTree
+invertWord64 =
+  testPropertyCompat "1 == x * invert x" "invertWord64" $
+    invert' @18446744073709551557 @Word64
+
+wordProps :: TestTree
+wordProps =
+  testGroup
+    "Word"
+    [ mkModPWord,
+      mkModPFailLargeWord,
+      mkModPFailCompositeWord,
+      addTotalWord,
+      subTotalWord,
+      multTotalWord,
+      divTotalWord,
+      invertWord
+    ]
+
+mkModPWord :: TestTree
+mkModPWord =
+  testPropertyCompat "mkModP x" "mkModNWord" $
+    mkModP' @65521 @Word
+
+mkModPFailLargeWord :: TestTree
+mkModPFailLargeWord =
+  testPropertyCompat "mkModP fails too large" "mkModPFailLargeWord" $
+    mkModPFailSize @18446744073709551629 @Word "Word" "18446744073709551615" "18446744073709551629"
+
+mkModPFailCompositeWord :: TestTree
+mkModPFailCompositeWord =
+  testPropertyCompat "mkModP fails for non-prime" "mkModPFailCompositeWord" $
+    mkModPFailComposite @65517 @Word "65517"
+
+addTotalWord :: TestTree
+addTotalWord =
+  testPropertyCompat "(.+.) implements modular addition" "addTotalWord" $
+    addTotal' @65521 @Word
+
+subTotalWord :: TestTree
+subTotalWord =
+  testPropertyCompat "(.-.) implements modular subtraction" "subTotalWord" $
+    subTotal' @65521 @Word
+
+multTotalWord :: TestTree
+multTotalWord =
+  testPropertyCompat "(.*.) implements modular multiplication" "multTotalWord" $
+    multTotal' @65521 @Word
+
+divTotalWord :: TestTree
+divTotalWord =
+  testPropertyCompat "(.%.) implements modular division" "divTotalWord" $
+    divTotal' @65521 @Word
+
+invertWord :: TestTree
+invertWord =
+  testPropertyCompat "1 == x * invert x" "invertWord" $
+    invert' @65521 @Word
+
+#else
+
+extraIntProps = []
+
+extraWordProps = []
+
+#endif
